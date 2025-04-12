a29aa03bee9159d2c81e5886e7355ccc7ebcaf47
VRM <vromanm@mail.ru>
2025-04-12 19:08:40 +0300

    Initial Commit

---
+++ FitNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitNote
+//
+//  Created by Роман Вертячих on 12.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
